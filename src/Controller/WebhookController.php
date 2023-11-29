094700dff5023d6bea5914156c5944606db1d1ac
Eaulmesse <theo.guerin01120@gmail.com>
2023-11-29 16:28:12 +0100

    up to date

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -3,11 +3,17 @@
 namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
+use App\Repository\UsersRepository;
 use App\Repository\AddressesRepository;
 use App\Repository\CompaniesRepository;
+use App\Repository\ContractsRepository;
+use App\Repository\EmployeesRepository;
+use App\Repository\QuotationsRepository;
 use App\Service\AddressesWebhookService;
 use App\Service\CompaniesWebhookService;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Service\ContractsWebhookService;
+use App\Service\EmployeesWebhookService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +26,22 @@ class WebhookController extends AbstractController
     public function callWebhookCompanies(Request $request, SessionInterface $session, LoggerInterface $logger, EntityManagerInterface $em, CompaniesRepository $companiesRepository, AddressesRepository $addressesRepository, CompaniesWebhookService $companiesWebhookService, AddressesWebhookService $addressesWebhookService): Response
     {
         $data = $companiesWebhookService->getWebhookCompanies($request, $session, $logger, $em, $companiesRepository, $addressesRepository, $addressesWebhookService);
+
+        return $data;
+    }
+
+    #[Route('/webhook/contracts', name: 'app_webhook_employees')]
+    public function callWebhookContracts(Request $request, SessionInterface $session, EntityManagerInterface $em, LoggerInterface $logger, ContractsRepository $contractsRepository, UsersRepository $usersRepository, CompaniesRepository $companiesRepository, AddressesRepository $addressesRepository, QuotationsRepository $quotationsRepository, ContractsWebhookService $contractsWebhookService): Response
+    {
+        $data = $contractsWebhookService->getWebhookContracts($request, $session, $em, $logger, $contractsRepository, $usersRepository, $companiesRepository, $addressesRepository, $quotationsRepository);
+
+        return $data;
+    }
+
+    #[Route('/webhook/employees', name: 'app_webhook_employees', methods: 'POST')]
+    public function callWebhookEmployees(Request $request, SessionInterface $session, EntityManagerInterface $em, LoggerInterface $logger, EmployeesRepository $employeesRepository, EmployeesWebhookService $employeesWebhookService): Response
+    {
+        $data = $employeesWebhookService->getWebhookEmployees($request, $session, $em, $logger, $employeesRepository);
 
         return $data;
     }
---
+++ src/Service/EmployeesWebhookService.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Companies;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EmployeesRepository;
+use App\Entity\Employees;
+
+
+
+class EmployeesWebhookService
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $webhookLogger)
+    {
+        $this->logger = $webhookLogger;
+    }
+
+    public function getWebhookEmployees(Request $request, SessionInterface $session, EntityManagerInterface $em, LoggerInterface $logger, EmployeesRepository $employeesRepository): Response
+    {
+        $response = json_decode($request->getContent(), true);
+
+        if ($response === null) {
+            $this->logger->error('Invalid JSON received.');
+            return new Response('Invalid JSON', Response::HTTP_BAD_REQUEST);
+        }
+
+        $session->set('webhook_data', $response);
+        
+        $this->logger->info('Webhook Employees received!', $response);
+
+        $this->webhookEmployeesFilter($session, $em, $logger, $employeesRepository);
+
+        return new Response('Received!', Response::HTTP_OK);
+    }
+
+    public function creatingEmployees(SessionInterface $session, EntityManagerInterface $em): Response
+    {
+        $webhookData = $session->get('webhook_data');
+
+        $this->logger->INFO('Création: ', $webhookData);
+
+        $companyId = $webhookData["data"]["company_id"];
+        $company = $em->getRepository(Companies::class)->find($companyId);
+            
+        $dataEmployees = new Employees();
+
+        $dataEmployees->setId($webhookData["data"]["id"]);
+        $dataEmployees->setGender($webhookData["data"]["gender"]);
+        $dataEmployees->setFirstname($webhookData["data"]["firstname"]);
+        $dataEmployees->setLastname($webhookData["data"]["lastname"]);
+        $dataEmployees->setEmail($webhookData["data"]["email"]);
+        $dataEmployees->setPhoneNumber($webhookData["data"]["phone_number"]);
+        $dataEmployees->setCellphoneNumber($webhookData["data"]["cellphone_number"]);
+        $dataEmployees->setJob($webhookData["data"]["job"]);
+        $dataEmployees->setIsBillingContact($webhookData["data"]["is_billing_contact"]);
+        $dataEmployees->setCompany($company);
+
+        $em->persist($dataEmployees);
+           
+        try{
+            $em->flush();
+        }
+        catch(\Exception $e){
+            error_log($e->getMessage());
+        }
+
+        return new Response(' Done!', Response::HTTP_OK);
+    }
+
+    public function updatingEmployees(SessionInterface $session, EntityManagerInterface $em): Response
+    {
+        $webhookData = $session->get('webhook_data');
+        $this->logger->INFO('Modification: ', $webhookData);
+
+        $updatedEmployee = $em->getRepository(Employees::class)->find($webhookData["data"]["id"]);
+        
+        $em->remove($updatedEmployee);
+        $em->flush();
+
+        $companyId = $webhookData["data"]["company_id"];
+        $company = $em->getRepository(Companies::class)->find($companyId);
+
+        $dataEmployees = new Employees();
+
+        $dataEmployees->setId($webhookData["data"]["id"]);
+        $dataEmployees->setGender($webhookData["data"]["gender"]);
+        $dataEmployees->setFirstname($webhookData["data"]["firstname"]);
+        $dataEmployees->setLastname($webhookData["data"]["lastname"]);
+        $dataEmployees->setEmail($webhookData["data"]["email"]);
+        $dataEmployees->setPhoneNumber($webhookData["data"]["phone_number"]);
+        $dataEmployees->setCellphoneNumber($webhookData["data"]["cellphone_number"]);
+        $dataEmployees->setJob($webhookData["data"]["job"]);
+        $dataEmployees->setIsBillingContact($webhookData["data"]["is_billing_contact"]);
+        $dataEmployees->setCompany($company);
+        
+        $em->persist($dataEmployees);
+       
+        try{
+            $em->flush();
+        }
+        catch(\Exception $e){
+            error_log($e->getMessage());
+        }
+
+        return new Response(' Done!', Response::HTTP_OK);
+    }
+
+    public function deletingEmployees(SessionInterface $session, EntityManagerInterface $em): Response
+    {
+        $webhookData = $session->get('webhook_data');
+        $this->logger->INFO('Suppression: ', $webhookData);
+
+        $updatedEmployee = $em->getRepository(Employees::class)->find($webhookData["data"]["id"]);
+        
+        $em->remove($updatedEmployee);
+        $em->flush();
+        return new Response(' Done!', Response::HTTP_OK);
+    }
+
+    #[Route('/webhook/companies/filter', name: 'app_webhook_companies_filter')]
+    public function webhookEmployeesFilter(SessionInterface $session, EntityManagerInterface $em, LoggerInterface $logger): Response 
+    {
+        $webhookData = $session->get('webhook_data');
+        
+        if (isset($webhookData['topic']) && $webhookData['topic'] === 'employee.created') {
+
+            $this->creatingEmployees($session, $em);
+            
+        }  
+        else if(isset($webhookData['topic']) && $webhookData['topic'] === 'employee.updated') {
+
+            $this->updatingEmployees($session, $em);
+            
+        }
+        else if(isset($webhookData['topic']) && $webhookData['topic'] === 'employee.deleted') {
+        
+            $this->deletingEmployees($session, $em);
+        }
+
+        return new Response(' Done!', Response::HTTP_OK);
+    }
+}
+
+
+
+
+
+
